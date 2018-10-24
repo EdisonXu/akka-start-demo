c2e823c7a677c83cca62d6c898582934d33390da
Edison <edisonxu@outlook.com>
2018-10-24 17:24:04 +0800

    Init

---
+++ cluster/src/main/java/com/ex/demo/App.java
@@ -0,0 +1,13 @@
+package com.ex.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
